f972b1f1d2796542bea4036acdb10bfbad6ca20a
Noah Menikefs <nmenikefs@gmail.com>
2020-06-15 15:52:27 -0400

    push

--- MeniSked-api/server.js
+++ MeniSked-api/server.js
@@ -30,210 +30,210 @@ app.use(express.json());
 app.use(cors())
 
 
-const database = {
-	users: [
-		{
-			id: 1,
-			firstname: 'John',
-			lastname: 'Smith',
-			password: 'cookies',
-			email: 'john@gmail.com',
-			colour: '#FFD678',
-			department: 'ST-JOES-A',
-			isadmin: false,
-			isactive: true,
-			worksked: [
-				{
-					id: 0,
-					date: "05/29/2020"
-				},
-				{
-					id: 8,
-					date: "08/7/2020"
-				}
-			]
-		},
-		{
-			id: 2,
-			firstname: 'Sally',
-			lastname: 'Jenkins',
-			password: 'bananas',
-			email: 'sally@gmail.com',
-			colour: '#F49F93',
-			department: 'ST-JOES-A',
-			isadmin: false,
-			isactive: true,
-			worksked: [
-				{
-					id: 2,
-					date: "06/9/2020"
-				},
-				{
-					id: 4,
-					date: "06/21/2020"
-				}
-			]
-		},
-		{
-			id: 0,
-			firstname: 'Peter',
-			lastname: 'Menikefs',
-			password: '123',
-			email: 'pnm@gmail.com',
-			colour: '#FFEE96',
-			department: 'ST-JOES-A Admin',
-			isadmin: true,
-			isactive: true,
-			worksked: [
-				{
-					id: 2,
-					date: "07/19/2020"
-				},
-				{
-					id: 3,
-					date: "06/29/2020"
-				}
-			]
-		}
-	],
-	login: [
-		{
-			id: '100',
-			hash: '',
-			email: 'john@gmail.com'
-		}
-	],
-	holidays: [
-		[
-			{
-				name: "New Year's Day",
-				isactive: true,
-				month: "January",
-				day: 1
-			},
-			{
-				name: "Christmas Day",
-				isactive: true,
-				month: "December",
-				day: 25
-			}
-		],
-		[
-			{
-				name: "Party",
-				eventsked: ["12/21/2020","01/12/2021"]
-			},
-			{
-				name: "Easter",
-				eventsked: ["04/12/2020","04/04/2021"]
-			}
+// const database = {
+// 	users: [
+// 		{
+// 			id: 1,
+// 			firstname: 'John',
+// 			lastname: 'Smith',
+// 			password: 'cookies',
+// 			email: 'john@gmail.com',
+// 			colour: '#FFD678',
+// 			department: 'ST-JOES-A',
+// 			isadmin: false,
+// 			isactive: true,
+// 			worksked: [
+// 				{
+// 					id: 0,
+// 					date: "05/29/2020"
+// 				},
+// 				{
+// 					id: 8,
+// 					date: "08/7/2020"
+// 				}
+// 			]
+// 		},
+// 		{
+// 			id: 2,
+// 			firstname: 'Sally',
+// 			lastname: 'Jenkins',
+// 			password: 'bananas',
+// 			email: 'sally@gmail.com',
+// 			colour: '#F49F93',
+// 			department: 'ST-JOES-A',
+// 			isadmin: false,
+// 			isactive: true,
+// 			worksked: [
+// 				{
+// 					id: 2,
+// 					date: "06/9/2020"
+// 				},
+// 				{
+// 					id: 4,
+// 					date: "06/21/2020"
+// 				}
+// 			]
+// 		},
+// 		{
+// 			id: 0,
+// 			firstname: 'Peter',
+// 			lastname: 'Menikefs',
+// 			password: '123',
+// 			email: 'pnm@gmail.com',
+// 			colour: '#FFEE96',
+// 			department: 'ST-JOES-A Admin',
+// 			isadmin: true,
+// 			isactive: true,
+// 			worksked: [
+// 				{
+// 					id: 2,
+// 					date: "07/19/2020"
+// 				},
+// 				{
+// 					id: 3,
+// 					date: "06/29/2020"
+// 				}
+// 			]
+// 		}
+// 	],
+// 	login: [
+// 		{
+// 			id: '100',
+// 			hash: '',
+// 			email: 'john@gmail.com'
+// 		}
+// 	],
+// 	holidays: [
+// 		[
+// 			{
+// 				name: "New Year's Day",
+// 				isactive: true,
+// 				month: "January",
+// 				day: 1
+// 			},
+// 			{
+// 				name: "Christmas Day",
+// 				isactive: true,
+// 				month: "December",
+// 				day: 25
+// 			}
+// 		],
+// 		[
+// 			{
+// 				name: "Party",
+// 				eventsked: ["12/21/2020","01/12/2021"]
+// 			},
+// 			{
+// 				name: "Easter",
+// 				eventsked: ["04/12/2020","04/04/2021"]
+// 			}
 
-		]
-	],
-	entries: [
-		{
-			id: 0,
-			name: "Request No Call",
-			isactive: true
-		},
-		{
-			id: 1,
-			name: "No Assignment",
-			isactive: true
-		},
-		{
-			id: 2,
-			name: "Not Available",
-			isactive: true
-		},
-		{
-			id: 3,
-			name: "Vacation",
-			isactive: true
-		},
-		{
-			id: 4,
-			name: "Staycation",
-			isactive: true
-		},
-		{
-			id: 5,
-			name: "Not Available Night",
-			isactive: true
-		},
-		{
-			id: 6,
-			name: "Assign Specific Call",
-			isactive: true
-		}
+// 		]
+// 	],
+// 	entries: [
+// 		{
+// 			id: 0,
+// 			name: "Request No Call",
+// 			isactive: true
+// 		},
+// 		{
+// 			id: 1,
+// 			name: "No Assignment",
+// 			isactive: true
+// 		},
+// 		{
+// 			id: 2,
+// 			name: "Not Available",
+// 			isactive: true
+// 		},
+// 		{
+// 			id: 3,
+// 			name: "Vacation",
+// 			isactive: true
+// 		},
+// 		{
+// 			id: 4,
+// 			name: "Staycation",
+// 			isactive: true
+// 		},
+// 		{
+// 			id: 5,
+// 			name: "Not Available Night",
+// 			isactive: true
+// 		},
+// 		{
+// 			id: 6,
+// 			name: "Assign Specific Call",
+// 			isactive: true
+// 		}
 
 
 
-	],
-	callTypes: [
-		{
-			id: 7,
-			name: "1st Call Day",
-			priority: 1,
-			active: true
-		},
-		{
-			id: 8,
-			name: "1st Call Night",
-			priority: 2,
-			active: true
-		},
-		{
-			id: 9,
-			name: "2nd Call Day",
-			priority: 3,
-			active: true
-		}
-	],
-	messages: [
-		{
-			userId: 1,
-			status: "pending",
-			dateSent: "07/10/2020",
-			requestDates: [
-				"07/17/2020"
-			],
-			typeID: 0,
-			response: ""
-		},
-		{	userId: 3,
-			status: "denied",
-			dateSent: "07/02/2020",
-			requestDates: [
-				"07/19/2020", "07/20/2020", "07/21/2020"
-			],
-			typeID: 2,
-			response: "Sorry, I can't let you work that day"
-		}
-	],
-	notes: [
-		{
-			id: 1,
-			type: 1,
-			date: "05/2/2020",
-			msg: "0  2  +1"
-		},
-		{
-			id: 2,
-			type: 2,
-			date: "05/21/2020",
-			msg: "Armogan + 1"
-		},
-		{
-			id: 3,
-			type: 3,
-			date: "05/25/2020",
-			msg: "busy day"
-		}
-	],
-	colours: ['#FFEE9680','#F49F9380','#FFD67880','#EB040080','#FFE70080','#FF56C580','#FFAC3E80','#EC019180','#F8831C80','#D980FF80','#B60EFF80','#83E5C780','#7101A980','#57BB7E80','#0091FF80','#19735B80','#88E4FF80','#C7EF6580','#41A3CC80','#85C63580'],
-	published: 0 //Months after June 2020
-}
+// 	],
+// 	callTypes: [
+// 		{
+// 			id: 7,
+// 			name: "1st Call Day",
+// 			priority: 1,
+// 			active: true
+// 		},
+// 		{
+// 			id: 8,
+// 			name: "1st Call Night",
+// 			priority: 2,
+// 			active: true
+// 		},
+// 		{
+// 			id: 9,
+// 			name: "2nd Call Day",
+// 			priority: 3,
+// 			active: true
+// 		}
+// 	],
+// 	messages: [
+// 		{
+// 			userId: 1,
+// 			status: "pending",
+// 			dateSent: "07/10/2020",
+// 			requestDates: [
+// 				"07/17/2020"
+// 			],
+// 			typeID: 0,
+// 			response: ""
+// 		},
+// 		{	userId: 3,
+// 			status: "denied",
+// 			dateSent: "07/02/2020",
+// 			requestDates: [
+// 				"07/19/2020", "07/20/2020", "07/21/2020"
+// 			],
+// 			typeID: 2,
+// 			response: "Sorry, I can't let you work that day"
+// 		}
+// 	],
+// 	notes: [
+// 		{
+// 			id: 1,
+// 			type: 1,
+// 			date: "05/2/2020",
+// 			msg: "0  2  +1"
+// 		},
+// 		{
+// 			id: 2,
+// 			type: 2,
+// 			date: "05/21/2020",
+// 			msg: "Armogan + 1"
+// 		},
+// 		{
+// 			id: 3,
+// 			type: 3,
+// 			date: "05/25/2020",
+// 			msg: "busy day"
+// 		}
+// 	],
+// 	colours: ['#FFEE9680','#F49F9380','#FFD67880','#EB040080','#FFE70080','#FF56C580','#FFAC3E80','#EC019180','#F8831C80','#D980FF80','#B60EFF80','#83E5C780','#7101A980','#57BB7E80','#0091FF80','#19735B80','#88E4FF80','#C7EF6580','#41A3CC80','#85C63580'],
+// 	published: 0 //Months after June 2020
+// }
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -392,7 +392,7 @@ app.put('/holiday/snr', (req,res) => {
 	db('nrholidays')
 		.where('name','=', name)
 		.update({
-			eventsked: knex.raw('array_append(eventsked, ?)', [month+'/'+day+'/'+year])
+			eventsked: db.raw('array_append(eventsked, ?)', [month+'/'+day+'/'+year])
 		})
 		.returning('*')
 		.then(all => {
