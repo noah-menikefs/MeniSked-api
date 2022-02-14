1ad122da4b0ebb1c5cc31fd9cd0d8e004646aa0d
Noah <31925525+noah-menikefs@users.noreply.github.com>
2022-02-13 21:31:53 -0500

    Update server.js

--- MeniSked-api/server.js
+++ MeniSked-api/server.js
@@ -35,15 +35,6 @@ const app = express();
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(cors())
-app.options('*', cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
