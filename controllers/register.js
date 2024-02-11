a086133b8cdc79efa278edb46507755b6a47ff91
Noah Menikefs <noah.menikefs@thescore.com>
2024-02-11 10:20:57 -0500

    logging

--- controllers/register.js
+++ controllers/register.js
@@ -36,7 +36,7 @@ const handleRegister = (req, res, db, bcrypt) => {
 				.then(trx.commit)
 				.catch(trx.rollback)
 			})
-			.catch(err => res.status(400).json('error while registering'));
+			.catch(err => res.status(400).json(err + ' . Error while registering'));
 		});
 	});
 }
